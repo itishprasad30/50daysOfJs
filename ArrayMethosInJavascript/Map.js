4e805fe81df7ae13b06f8781ec8c7f2f080846d2
itishprasad30 <itishprasad20@gmail.com>
2022-03-04 20:02:02 +0530

    inital commit

---
+++ ArrayMethosInJavascript/Map.js
@@ -0,0 +1,9 @@
+var val = 23;
+console.log(val);
+var string = "Hello";
+console.log(string);
+var person = {
+    name: "John",
+    age: 21
+};
+console.log(person);
---
+++ ArrayMethosInJavascript/Map.ts
@@ -0,0 +1,17 @@
+const value: number = 23;
+
+console.log(value);
+
+let welcome: string = "Hello";
+console.log(welcome);
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+const people:Person = {
+    name: "John",
+    age:12
+}
+console.log(people);
