65b6a47316c81bed82216b0e43ad2c08d8749a5a
itishprasad30 <itishprasad20@gmail.com>
2022-03-05 19:27:44 +0530

    includes()  and filter() provided

--- ArrayMethosInJavascript/HigherLabelArrayMethod.ts
+++ ArrayMethosInJavascript/HigherLabelArrayMethod.ts
@@ -9,4 +9,17 @@ arr.forEach(item => {
     return console.log(item )
 })
 
+// 2 - includes()
+// check if arra includes the item passed in this method 
+
+let isHere = arr.includes(4)
+console.log(isHere) // true
+
+
+// 3 - filter()
+// this method create a new array with only elements passed condition inside the provided function
+
+const filteredValue:number[] = arr.filter(item => item >= 5)
+console.log(filteredValue) // 56
 console.log(arr)
+
