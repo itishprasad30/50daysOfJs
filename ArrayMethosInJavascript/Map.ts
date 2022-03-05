e929bcabb9ea4b3926790f75055acc97aaa0b0cc
itishprasad30 <itishprasad20@gmail.com>
2022-03-05 12:20:56 +0530

    data added

--- ArrayMethosInJavascript/Map.ts
+++ ArrayMethosInJavascript/Map.ts
@@ -15,3 +15,26 @@ const people:Person = {
     age:12
 }
 console.log(people);
+
+
+interface Data {
+    artist:boolean;
+    age:number;
+    persons:string[];
+    price:number[]
+}
+
+let data:Data = {
+    artist:true,
+    age:20,
+    persons:['hailee Stanfield','The Weeknd','Taylor Swift','Ed shreen']   ,
+    price:[1,2,3,4,5,6,7,8,9,10]
+
+}
+
+const reduceData = data.price.reduce((a,b ) => Math.floor((a+b )/2) ,0 )
+console.log(reduceData)
+
+let filterData:string[] = data.persons.filter(name => name.toLowerCase() === 'ed shreen')
+
+console.log(filterData)
