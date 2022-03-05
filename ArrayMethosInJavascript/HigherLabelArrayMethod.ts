3538c9058efa144f92d39488a33a9ed4242f3560
itishprasad30 <itishprasad20@gmail.com>
2022-03-05 19:09:24 +0530

    forEach () added

---
+++ ArrayMethosInJavascript/HigherLabelArrayMethod.ts
@@ -0,0 +1,12 @@
+// Top 10 Higher Label Array Methods 
+
+// 1 - forEach()
+// helps to loops through an array of items
+
+const arr = [1,2,3,4,5,6]
+
+arr.forEach(item => {
+    return console.log(item )
+})
+
+console.log(arr)
