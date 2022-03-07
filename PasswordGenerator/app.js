53bac24a1f7606d7a8ca1214cbb4dbd78bf7d6a0
itishprasad30 <itishprasad20@gmail.com>
2022-03-07 12:35:37 +0530

    finaly done the project

--- PasswordGenerator/app.js
+++ PasswordGenerator/app.js
@@ -1,5 +1,11 @@
 const characterAmountRange = document.getElementById("CharAmountRange");
 const characterAmountNumber = document.getElementById("CharAmountNumber");
+const lowrerCaseElement = document.getElementById("lowerCase");
+const upperCaseElement = document.getElementById("upperCase");
+const numberElement = document.getElementById("number");
+const symbolElement = document.getElementById("symbol");
+const passwordDisplayElement = document.getElementById("passwordDisplay");
+
 const form = document.getElementById("passwordGeneratorForm");
 
 characterAmountNumber.addEventListener("input", syncCharAmount);
@@ -13,10 +19,23 @@ function syncCharAmount(e) {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const characterAmount = characterAmountNumber.value;
+  const includeLowerCase = lowrerCaseElement.checked;
+  const includeUpperCase = upperCaseElement.checked;
+  const includeNumber = numberElement.checked;
+  const includeSymbol = symbolElement.checked;
+  const password = generatePassword(
+    characterAmount,
+    includeNumber,
+    includeSymbol,
+    includeLowerCase,
+    includeUpperCase
+  );
+  passwordDisplayElement.innerHTML = password;
 });
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const symbols = ["@", "*", "#", "%", "$"];
+const symbols = ["_", "*", "#", "%", "$"];
 
 const characterCodes = Array.from(Array(26)).map((_, i) => i + 97);
 // console.log(characterCodes);
@@ -50,8 +69,9 @@ const generatePassword = (
     const randomIndex = Math.floor(Math.random() * availableCharacters.length);
     password += availableCharacters[randomIndex];
   }
+  console.log(password);
   return password;
 };
 
-const result = generatePassword(10, true, true, true, true);
-console.log(result);
+// const result = generatePassword(10, true, true, true, true);
+// console.log(result);
