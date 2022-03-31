6213004eb04a1ddf3629915e24df7d4389f6e97c
itishprasad30 <itishprasad20@gmail.com>
2022-03-31 16:34:53 +0530

    final touchs

--- Namaste Javascript Course/Hosting In Js/app.js
+++ Namaste Javascript Course/Hosting In Js/app.js
@@ -1,8 +1,19 @@
 getName(); // javascript basic
 console.log(x); // undefined
 
+console.log(getName); // prints the actual code
+// getName2(); // error is not a funtion
+getName3(); // error is not a funtion
 var x = 13;
 
 function getName() {
   console.log("Javascript basic");
 }
+
+var getName2 = () => {
+  console.log("arrow funtion ");
+};
+
+var getName3 = function () {
+  console.log("funtion another way to defined");
+};
