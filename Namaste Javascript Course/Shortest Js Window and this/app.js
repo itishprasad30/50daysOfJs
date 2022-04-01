7cbeb36a64f9586a95a027a1e4818a46bfe8c612
itishprasad30 <itishprasad20@gmail.com>
2022-04-01 20:14:48 +0530

    undefined and not defined

--- Namaste Javascript Course/Shortest Js Window and this/app.js
+++ Namaste Javascript Course/Shortest Js Window and this/app.js
@@ -8,3 +8,15 @@ console.log(window.a); //10 this is global object
 //  console.log(d);// error
 console.log(this.a); // 10
 // every thing inside this is global obj but inside function is local
+
+// undefined and not defined
+var a;
+console.log(a); // undefined
+console.log(x); // error not defined
+a = 10; //
+console.log(a); // 10
+a = "hello ";
+console.log(a); // hello
+
+// javascript is also called loosely type language because it dynalmically allocates the type to any variable
+//  also it called week language
