ad29a3a0712ee7fe047b74cf206f513275b93889
itishprasad30 <itishprasad20@gmail.com>
2022-04-03 21:16:33 +0530

    thorttle added

--- Namaste Javascript Course/debounce in javascript/script.js
+++ Namaste Javascript Course/debounce in javascript/script.js
@@ -1,16 +1,21 @@
 const input = document.querySelector("input");
 const defaultText = document.getElementById("default");
 const debouceText = document.getElementById("debouce");
-const thorttle = document.getElementById("thorttle");
+const thorttleText = document.getElementById("thorttle");
 
 input.addEventListener("input", (e) => {
   defaultText.textContent = e.target.value;
   updateDebouceText(e.target.value);
+  updateThorttleText(e.target.value);
 });
 
 const updateDebouceText = debouce((text) => {
   debouceText.textContent = text;
-}, 1000);
+});
+
+const updateThorttleText = thorttle((text) => {
+  thorttleText.textContent = text;
+});
 
 function debouce(cb, delay = 1000) {
   let timer;
@@ -19,5 +24,40 @@ function debouce(cb, delay = 1000) {
     timer = setTimeout(() => {
       cb(...args);
     }, delay);
+  };
+}
+
+function thorttle(cb, delay = 1000) {
+  let shouldWait = false;
+  let waitingArgs;
+
+  const timeoutFunc = () => {
+    if (waitingArgs == null) {
+      shouldWait = false;
+    } else {
+      cb(...waitingArgs);
+      waitingArgs = null;
+      setTimeout(timeoutFunc, delay);
+    }
+  };
+
+  return (...args) => {
+    if (shouldWait) {
+      waitingArgs = args;
+      return;
+    }
+
+    cb(...args);
+    shouldWait = true;
+
+    // setTimeout(() => {
+    //   if (waitingArgs == null) {
+    //     shouldWait = false;
+    //   } else {
+    //     cb(...waitingArgs);
+    //     waitingArgs = null;
+    //   }
+    // }, delay);
+    setTimeout(timeoutFunc, delay);
   };
 }
